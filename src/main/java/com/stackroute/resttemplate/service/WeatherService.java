d0a96d4ac8f098e77d62f92b0acfb6b0f705abfb
Rohit <rohit.1.kumar@stackroute.in>
2023-11-13 10:33:18 +0530

    init

---
+++ src/main/java/com/stackroute/resttemplate/service/WeatherService.java
@@ -0,0 +1,27 @@
+package com.stackroute.resttemplate.service;
+
+import com.stackroute.resttemplate.model.Weather;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    //add your api key here
+    private static final String API_KEY = "";
+
+    //add your api url here
+    private static final String API_URL = "";
+
+    private final RestTemplate restTemplate;
+    public WeatherService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    //using rest template, get the weather details of a city
+    public Weather getWeather(String city) {
+      return null;
+    }
+
+
+}
